7a078a81c0616ddb1ea5610059628190304d59f7
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-04-25 01:18:46 +0900

    firebase用ビルド設定の追加

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -2,5 +2,9 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+    ssr:false,
+    target:"static",
+    generate: {
+        dir: 'app'
+      }
 })
