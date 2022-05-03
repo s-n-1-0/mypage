a195156ce0a6199c06d6811c840b109749bdccec
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-05-03 22:03:49 +0900

    テンプレートエラー修正

--- nuxt/pages/apps/diary/updates.vue
+++ nuxt/pages/apps/diary/updates.vue
@@ -4,7 +4,7 @@
         <Link rel="shortcut icon" :href="iconRef"/>
         <Link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <Link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <Link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <Link rel="preconnect" href="https://fonts.gstatic.com" crossorigin=""/>
         <Link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap" rel="stylesheet" />
         <Link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"/>
     </Head>
