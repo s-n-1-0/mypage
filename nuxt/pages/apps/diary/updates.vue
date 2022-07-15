aafcec8f35815898312db9c35a68939985e2c152
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-07-15 23:13:37 +0900

    リダイレクト先ページに正規化タグを設置

--- nuxt/pages/apps/diary/updates.vue
+++ nuxt/pages/apps/diary/updates.vue
@@ -2,6 +2,7 @@
     <Head>
         <Title>ちくわ日記 - アップデート情報</Title>
         <Link rel="shortcut icon" :href="iconRef"/>
+        <Link rel="canonical" href= "https://hello.sn-10.net/apps/diary/updates"/>
         <Link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <Link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"/>
     </Head>
