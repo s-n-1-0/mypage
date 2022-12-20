f74e558c20343b94f479bc8e7ee7f434446915b5
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-12-21 00:57:27 +0900

    apps/diaryページを仮追加

---
+++ nuxt/pages/apps/diary/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <Head>
+    <Link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+      crossorigin="anonymous"
+    />
+    <Script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+      crossorigin="anonymous"
+    ></Script>
+  </Head>
+  <div class="container pt-2"></div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({});
+</script>
