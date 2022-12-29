225eff5dc7364e27cd02f74f882a946a9bb1b427
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-12-29 22:35:45 +0900

    ポートフォリオ画面のBootstrap削除(Link削除)

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -10,17 +10,7 @@
         crossorigin="anonymous"
         referrerpolicy="no-referrer"
       />
-      <Link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-        rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-        crossorigin="anonymous"
-      />
-      <Script
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous"
-      ></Script>
+
       <Script
         src="https://twemoji.maxcdn.com/v/latest/twemoji.min.js"
         crossorigin="anonymous"
