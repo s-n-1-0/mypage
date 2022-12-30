6ef5156b3db5ac1e0ee9f5fdf037686aa6a862e8
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-12-30 15:37:53 +0900

    timelineコンポーネントの色調整

--- nuxt/components/timeline.vue
+++ nuxt/components/timeline.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="timeline" class="mx-10">
-    <h1 class="text-2xl pt-1 font-bold" style="color: #dd1f1f">Activity</h1>
+    <h1 class="text-2xl pt-1 font-bold">Activity</h1>
     <hr />
-    <ol class="relative border-l border-gray-200 dark:border-gray-700">
+    <ol class="relative border-l border-gray-200">
       <a
         v-for="(item, i) in timelineItems"
         :key="i"
@@ -11,20 +11,17 @@
         :href="item.url"
       >
         <div
-          class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"
+          class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white"
         ></div>
-        <time
-          class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
-          >{{ getTimeText(item.pubDateMs) }}</time
-        >
-        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+        <time class="mb-1 text-sm font-normal leading-none text-gray-400">{{
+          getTimeText(item.pubDateMs)
+        }}</time>
+        <h3 class="text-lg font-semibold text-gray-900">
           <img :src="noteIconPath" class="object-cover w-10 inline" />{{
             item.title
           }}
         </h3>
-        <p
-          class="text-base font-normal text-gray-500 dark:text-gray-400 text-end"
-        >
+        <p class="text-base font-normal text-gray-500 text-end">
           (note, {{ getDiffTimeText(item.pubDateMs) }})
         </p>
       </a>
@@ -70,3 +67,8 @@ export default defineComponent({
   },
 });
 </script>
+<style scoped>
+h1 {
+  color: #ff2727;
+}
+</style>
