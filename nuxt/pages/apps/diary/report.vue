74ba49a7cf0d2991ecb77f0a563bf9f9040299e1
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2023-09-12 00:26:14 +0900

    フィードバック処理を追加

--- nuxt/pages/apps/diary/report.vue
+++ nuxt/pages/apps/diary/report.vue
@@ -18,27 +18,42 @@
         アプリを利用していて「こういう機能がほしい」や「ここを直して欲しい」など気づいたことがありましたらぜひお気軽に↓のフォームからお送りください!<br />
         改善点が見つかると非常に助かります。
         <div class="text-sm">
-          <p>※バグ報告の場合は詳細にお願いします!</p>
-          <a class="text-blue-600">※開発者からの返答</a>
-
-          <textarea
-            rows="4"
-            class="my-2 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
-            placeholder="ここに要望を記入..."
-          ></textarea>
-          <p class="text-center">
-            <button
-              class="app-color hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-            >
-              送信
-            </button>
-          </p>
+          <a
+            class="text-blue-600 underline"
+            href="https://scrapbox.io/diary10/%E9%81%8E%E5%8E%BB%E3%81%AE%E5%95%8F%E3%81%84%E5%90%88%E3%82%8F%E3%81%9B%E3%81%B8%E3%81%AE%E5%9B%9E%E7%AD%94"
+            >※開発者からの返答</a
+          >
         </div>
+        <textarea
+          rows="4"
+          class="my-2 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+          placeholder="ここにメッセージを記入..."
+          v-model="message"
+        ></textarea>
+        <p class="text-center">
+          <button
+            v-if="message != ''"
+            v-on:click="sendFeedback"
+            class="app-color hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+          >
+            送信
+          </button>
+        </p>
       </div>
     </div>
   </div>
 </template>
-<script lang="ts"></script>
+<script lang="ts" setup>
+const message = toRef("");
+function sendFeedback() {
+  const route = useRoute();
+  sendAppFeedback({
+    ...route.query,
+    app_id: "ChikuwaDiary",
+    message: message.value,
+  });
+}
+</script>
 <style>
 .app-color {
   background-color: #4d6a87ff;
--- nuxt/utils/firebase.ts
+++ nuxt/utils/firebase.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+const functionsURl = "https://us-central1-apps-d802a.cloudfunctions.net/";
 const storageUrl = "https://storage.googleapis.com/apps-d802a.appspot.com/";
 export interface TimelineItem {
   title: string;
@@ -18,4 +19,12 @@ export async function getTestTimelinejson() {
   let res = await axios.get("test_timeline.json");
   let data: TimelineItem[] = res.data;
   return data;
+}
+export interface AppFeedbackBody {
+  app_id: "ChikuwaDiary";
+  message: String;
+  [prop: string]: any;
+}
+export async function sendAppFeedback(params: AppFeedbackBody) {
+  return await axios.post(functionsURl + "common/sendFeedback", params);
 }
