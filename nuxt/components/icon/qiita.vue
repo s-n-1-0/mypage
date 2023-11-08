4ca23f63942cc946bfa02d8b7a2c4eb625c4ee2f
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2023-11-09 03:20:14 +0900

    タイムラインにQiitaとZennの追加

--- nuxt/components/icon/qiita.vue
+++ nuxt/components/icon/qiita.vue
@@ -1,10 +1,10 @@
 <template>
   <img
     :src="iconPath"
-    class="object-contain w-9 h-100 inline"
+    class="object-contain w-9 h-100 inline mx-2"
     style="max-width: inherit"
   />
 </template>
 <script lang="ts" setup>
-import iconPath from "@/assets/sub/links/connpass-icon.png";
+import iconPath from "@/assets/sub/links/qiita-icon.png";
 </script>
