90c128e81c75116da86644504dc112e800cf3583
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2024-03-20 23:49:35 +0900

    NoteCardコンポーネントとして分離

--- nuxt/pages/apps/diary/updates.vue
+++ nuxt/pages/apps/diary/updates.vue
@@ -37,38 +37,8 @@
     </p>
     <div>
       <div class="section">
-        <div v-for="note in noteList.slice(0, 2)" class="w-full p-2">
-          <a
-            v-if="note.thumbnailUrl != ''"
-            :href="note.url"
-            class="mx-auto flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100"
-          >
-            <img
-              class="object-contain w-full rounded-t-lg h-auto md:h-full md:max-h-28 md:w-auto md:rounded-none md:rounded-l-lg"
-              :src="note.thumbnailUrl"
-              alt=""
-            />
-            <div class="flex flex-col justify-between p-4 leading-normal">
-              <h5
-                class="mb-2 text-xl font-bold tracking-tight text-gray-900 line-clamp-2"
-              >
-                {{ note.title }}
-              </h5>
-              <!-- <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                content
-              </p> -->
-            </div>
-          </a>
-          <div v-else>
-            <div
-              class="mx-auto block md:max-w-xl p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100"
-            >
-              <p
-                class="font-normal text-gray-700 dark:text-gray-400"
-                v-html="fixDescriptionHtml(note.descriptionHtml)"
-              ></p>
-            </div>
-          </div>
+        <div v-for="note in noteList.slice(0, 2)" class="p-2">
+          <NoteCard :note="note" />
         </div>
       </div>
       <div class="section">
@@ -81,37 +51,7 @@
       </div>
       <div class="section">
         <div v-for="note in noteList.slice(2)" class="w-full p-2">
-          <a
-            v-if="note.thumbnailUrl != ''"
-            :href="note.url"
-            class="mx-auto flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100"
-          >
-            <img
-              class="object-contain w-full rounded-t-lg h-auto md:h-full md:max-h-28 md:w-auto md:rounded-none md:rounded-l-lg"
-              :src="note.thumbnailUrl"
-              alt=""
-            />
-            <div class="flex flex-col justify-between p-4 leading-normal">
-              <h5
-                class="mb-2 text-xl font-bold tracking-tight text-gray-900 line-clamp-2"
-              >
-                {{ note.title }}
-              </h5>
-              <!-- <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                content
-              </p> -->
-            </div>
-          </a>
-          <div v-else>
-            <div
-              class="mx-auto block md:max-w-xl p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100"
-            >
-              <p
-                class="font-normal text-gray-700 dark:text-gray-400"
-                v-html="fixDescriptionHtml(note.descriptionHtml)"
-              ></p>
-            </div>
-          </div>
+          <NoteCard :note="note" />
         </div>
       </div>
       <div class="section">
@@ -166,14 +106,6 @@ export default defineComponent({
     return {
       noteList: noteListRef,
       iconRef, //nuxt3 rc1でLikタグでアイコンを指定できないため仮処置
-      fixDescriptionHtml: (descriptionHtml: string) => {
-        return descriptionHtml
-          .replaceAll("\n", "<br>")
-          .replace(
-            "<a href='https://note.com",
-            "<br><br><a class='inline-flex items-center px-3 py-2 text-sm font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800' href='https://note.com"
-          );
-      },
     };
   },
 });
