21d05033106886b47d2f7c488f382a8ae1292e5d
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2024-12-20 13:38:22 +0900

    ヘルプページのリンクをScrapboxからNotionに変える

--- nuxt/pages/apps/diary/feedback.vue
+++ nuxt/pages/apps/diary/feedback.vue
@@ -17,13 +17,6 @@
       />
       <div style="max-width: 500px" class="mx-auto">
         <div v-html="localizedText.context"></div>
-        <div class="text-sm">
-          <a
-            class="text-blue-600 underline"
-            href="https://scrapbox.io/diary10/%E9%81%8E%E5%8E%BB%E3%81%AE%E5%95%8F%E3%81%84%E5%90%88%E3%82%8F%E3%81%9B%E3%81%B8%E3%81%AE%E5%9B%9E%E7%AD%94"
-            >{{ localizedText.reply }}</a
-          >
-        </div>
         <div v-if="!(sendingMode == 'now')">
           <textarea
             rows="4"
@@ -53,9 +46,33 @@
           <FontAwesomeIcon :icon="['fas', 'thumbs-up']" />
         </div>
       </div>
+      <div class="text-center mt-5">
+        <a
+          href="https://fast-magpie-e3a.notion.site/FAQ-11b397b7de8e807494aafc4a7ff7d261"
+          class="inline-flex items-center justify-center p-5 text-base font-medium text-gray-500 rounded-lg bg-gray-50 hover:text-gray-900 hover:bg-gray-100"
+        >
+          <span class="w-full">{{ localizedText.reply }}</span>
+          <svg
+            class="w-4 h-4 ms-2 rtl:rotate-180"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 10"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M1 5h12m0 0L9 1m4 4L9 9"
+            />
+          </svg>
+        </a>
+      </div>
     </div>
   </div>
 </template>
+
 <script lang="ts" setup>
 import enText from "@/assets/json/localize/apps/diary/en.json";
 import jaText from "@/assets/json/localize/apps/diary/ja.json";
